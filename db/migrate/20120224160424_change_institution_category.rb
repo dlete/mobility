0971693c45939143d6183a9c5fa1a5965fb54c0b
Daniel Lete <daniel.lete@gmail.com>
2012-02-24 21:49:01 +0000

    delete migration changing the table Institutions

--- db/migrate/20120224160424_change_institution_category.rb
+++
@@ -1,9 +0,0 @@
-class ChangeInstitutionCategory < ActiveRecord::Migration
-  def up
-    rename_column :institutions, :category_id, :institution_category_id
-  end
-
-  def down
-    rename_column :institutions, :institution_category_id, :category_id
-  end
-end
